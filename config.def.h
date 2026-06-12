fd521c1c18055e6e08355f542e0530dd7b4fab69
kullbachxyz <ph@kullbach.net>
2026-06-12 11:08:05 +0200

    switch to catppuccin colors

--- config.def.h
+++ config.def.h
@@ -3,13 +3,13 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] 	    = { "monospace:size=11", "Noto Color Emoji:size=11;antialias=true;autohint=true", };
+static const char *fonts[] 	    = { "monospace:size=13", "Iosevka Nerd Font Propo:size=16", "Noto Color Emoji:size=13;antialias=true;autohint=true", };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#cdd6f4", "#313244" },
+	[SchemeOut] = { "#11111b", "#94e2d5" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
