1c5da551153211ff3f4ece6ee221d9011051bdcd
kullbachxyz <ph@kullbach.net>
2026-08-13 12:40:29 +0200

    add center patch (spotlight)

--- config.def.h
+++ config.def.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c  option; centers dmenu on screen (Spotlight) */
+static int min_width = 600;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 /* Same monospace as dwm bar / st (IBM Plex Mono). Iosevka Nerd Font MUST
  * come before Noto Color Emoji, else emoji grabs the Nerd PUA glyphs first. */
